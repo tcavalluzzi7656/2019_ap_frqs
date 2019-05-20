e77658d0ac4978ce969c8b9240c8ef38311416b8
tcavalluzzi7656 <tcavalluzzi7656@bths.edu>
2019-05-20 14:43:52 -0400

    steptracker

--- src/StepTracker.java
+++ src/StepTracker.java
@@ -1,2 +1,34 @@
 public class StepTracker {
+    private int days;
+    private int activeDays;
+    private int totalSteps;
+    private int minSteps;
+
+    public StepTracker(int minSteps)
+    {
+        this.minSteps=minSteps;
+        this.days=0;
+        this.activeDays=0;
+        this.totalSteps=0;
+    }
+
+    public void addDailySteps(int num)
+    {
+        totalSteps+= num;
+        days++;
+        if(num>=minSteps)
+        {
+            activeDays++;
+        }
+    }
+
+    public int activeDays()
+    {
+        return activeDays;
+    }
+
+    public double averageSteps()
+    {
+        return ((double)totalSteps/(double)days);
+    }
 }
