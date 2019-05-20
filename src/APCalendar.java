edb55bc7056f45f7e5cb26292a536682ebc02b0b
tcavalluzzi7656 <tcavalluzzi7656@bths.edu>
2019-05-20 14:49:00 -0400

    steptracker

--- src/APCalendar.java
+++ src/APCalendar.java
@@ -1,2 +1,10 @@
 public class APCalendar {
+    private static boolean isLeapYear(int year)
+    {
+        if(year%4==0)
+        {
+            return  true;
+        }
+        return false;
+    }
 }
